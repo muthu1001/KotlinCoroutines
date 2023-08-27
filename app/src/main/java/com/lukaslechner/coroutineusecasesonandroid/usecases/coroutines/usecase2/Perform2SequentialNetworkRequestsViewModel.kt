dfa057a7826d2099c7b39c875f26f2709e833d08
muthu1001 <muthukumarmanoharan1001@gmail.com>
2023-08-27 20:27:50 +0530

    coroutines implementation practiced

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,14 +1,26 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
-        // TODO: Exercise 1
-        // switch to branch "coroutine_course_full" to see solution
+        uiState.postValue(UiState.Loading)
+        viewModelScope.launch {
+            try {
+                val androidVersion = mockApi.getRecentAndroidVersions()
+                val latestFeatures =
+                    mockApi.getAndroidVersionFeatures(androidVersion.last().apiLevel)
+                uiState.postValue(UiState.Success(latestFeatures))
+            } catch (ex: Exception) {
+                uiState.postValue(UiState.Error("Network error in fetching android features"))
+            }
+        }
     }
 }
