a5b5e31a8a238884a32c0c9276dd79cd2a2b3f14
muthu1001 <muthukumarmanoharan1001@gmail.com>
2023-08-27 20:17:10 +0530

    RxJava implementation practiced

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/rx/SequentialNetworkRequestsRxViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/rx/SequentialNetworkRequestsRxViewModel.kt
@@ -1,12 +1,36 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2.rx
 
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 
 class SequentialNetworkRequestsRxViewModel(
     private val mockApi: RxMockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
-    fun perform2SequentialNetworkRequest() {
+    private val disposable = CompositeDisposable()
 
+    fun perform2SequentialNetworkRequest() {
+        uiState.postValue(UiState.Loading)
+        mockApi.getRecentAndroidVersions().flatMap { androidVersions ->
+            mockApi.getAndroidVersionFeatures(androidVersions.last().apiLevel)
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread()).subscribeBy(
+                onSuccess = {
+                    uiState.postValue(UiState.Success(it))
+                },
+                onError = {
+                    uiState.postValue(UiState.Error("Error in getting latest android features"))
+                }
+            ).addTo(disposable)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
     }
 }
