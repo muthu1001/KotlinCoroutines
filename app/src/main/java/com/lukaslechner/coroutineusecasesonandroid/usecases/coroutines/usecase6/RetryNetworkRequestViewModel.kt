1b0f940f7d7b2948209e3d5c682a5170f3122954
muthu1001 <muthukumarmanoharan1001@gmail.com>
2023-10-18 07:39:58 +0530

    timeout retry expo backoff practise aligned

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -16,47 +16,55 @@ class RetryNetworkRequestViewModel(
     fun performNetworkRequest() {
         viewModelScope.launch {
             uiState.postValue(UiState.Loading)
-            val result :List<AndroidVersion>?= retriesWithExponentialBackOff(5,::getAndroidVersion)
-            if (result!=null){
-                uiState.postValue(UiState.Success(result!!))
-            }else{
+            val result: List<AndroidVersion>? =
+                retriesWithExponentialBackOff(5, ::getAndroidVersion)
+            if (result != null) {
+                uiState.postValue(UiState.Success(result))
+            } else {
                 uiState.postValue(UiState.Error("Network req failed"))
             }
         }
     }
 
-    private suspend fun <T> retriesWithExponentialBackOff(retires: Int, block: suspend ()->T?,initialDelay:Long = 100L,maxDelay:Long = 1000L,delayMultiplier:Int=2): T?{
+    private suspend fun <T> retriesWithExponentialBackOff(
+        retires: Int,
+        block: suspend () -> T?,
+        initialDelay: Long = 100L,
+        maxDelay: Long = 1000L,
+        delayMultiplier: Int = 2
+    ): T? {
         var result: T? = null
         var delayTime = initialDelay
-         repeat(retires-1){
+        repeat(retires - 1) {
             try {
-                 result = block()
-                    if(result!=null)
-                        return@repeat
-            }catch (ex: Exception){
+                result = block()
+                if (result != null) {
+                    return@repeat
+                }
+            } catch (ex: Exception) {
                 Timber.e(ex)
             }
-             delay(delayTime)
-             delayTime = getDelayTime(initialDelay,maxDelay,delayMultiplier*(it+1))
+            delay(delayTime)
+            delayTime = getDelayTime(initialDelay, maxDelay, delayMultiplier * (it + 1))
         }
-        return if(result!=null)
+        return if (result != null)
             result
         else block()
     }
 
     private fun getDelayTime(initialDelay: Long, maxDelay: Long, delayMultiplier: Int): Long {
-        val newDelay = initialDelay*delayMultiplier
-        println("delay for next ${if(newDelay>maxDelay) maxDelay else newDelay}")
-        return if (newDelay>maxDelay) maxDelay else newDelay
+        val newDelay = initialDelay * delayMultiplier
+        println("delay for next ${if (newDelay > maxDelay) maxDelay else newDelay}")
+        return if (newDelay > maxDelay) maxDelay else newDelay
     }
 
 
-    private suspend fun getAndroidVersion(timeOut:Long = 1100):List<AndroidVersion>? {
+    private suspend fun getAndroidVersion(timeOut: Long = 1100): List<AndroidVersion>? {
         return try {
-            withTimeoutOrNull(timeOut){
+            withTimeoutOrNull(timeOut) {
                 api.getRecentAndroidVersions()
             }
-        }catch (ex: Exception){
+        } catch (ex: Exception) {
             Timber.e(ex)
             null
         }
